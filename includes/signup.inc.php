390e0ce21158c21b04a38ca28326a694247783ba
YasserDalali <yasser.dalali.personal@gmail.com>
2024-05-09 15:34:41 +0100

    data entry validation

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -11,6 +11,30 @@ if (isset($_POST['signup-submit'])) {
     $mail = $_POST['mail'];
     $pwd = $_POST['pwd'];
     $pwdr = $_POST['pwd-repeat'];
+    if (empty($username) || empty($mail) || empty($pwd) || empty($pwdr)) {
+        echo "empty field(s)";
+        exit();
+    }
+
+    else if (!filter_var($mail, "FILTER_VALIDATE_EMAIL")) 
+    {
+        echo "invalid mail";
+        exit();
+    }
+
+    else if (!preg_match("/^[a-zA-Z0-9]*$/", $username)) 
+    {
+        echo "invalid username";
+        exit();
+    }
 
 
+    else if ($pwd !== $pwdr) 
+    {
+        echo "please make sure you repeat the same password";
+        exit();
+    }
+
+    $sql = "SELECT uidUsers FROM users where uidUsers={$username} AND pwdUsers={$pwd}";
+     
 }
